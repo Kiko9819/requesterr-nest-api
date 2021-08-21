eea983048d5b9cc9dc2079fdacd3ae1eace8b074
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-22 00:18:05 +0300

    Add users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { usersProviders } from './users.providers';
+
+@Module({
+  controllers: [
+    UsersController
+  ],
+  providers: [
+    ...usersProviders
+  ],
+  exports: [
+    ...usersProviders
+  ]
+})
+export class UsersModule {}
