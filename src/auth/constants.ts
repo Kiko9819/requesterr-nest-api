55a133265a3db204f7be1771bb273c82dfa5bf07
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-22 00:14:23 +0300

    Add temp jwt constant for secret

---
+++ src/auth/constants.ts
@@ -0,0 +1,8 @@
+/**
+ * in a production system you must protect this key
+ * using appropriate measures such as a secrets vault,
+ * environment variable, or configuration service.
+ */
+export const jwtConstants = {
+    secret: 'secretKey'
+};
