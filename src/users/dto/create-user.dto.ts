31a4ccb27972358b35d6629968448e401cce085d
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-22 11:06:39 +0300

    Add validation to dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
     name: string;
+
+    @IsNotEmpty()
     username: string;
+    
+    @MinLength(8)
+    @IsNotEmpty()
     password: string;
 }
