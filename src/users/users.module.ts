5994542e21d9e2e344de4dde2d67a76a854d9d54
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-23 22:50:17 +0300

    Import mail module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MailModule } from 'src/mail/mail.module';
 import { UsersController } from './users.controller';
 import { usersProviders } from './users.providers';
 
 @Module({
+  imports: [
+    MailModule
+  ],
   controllers: [
     UsersController
   ],
   providers: [
-    ...usersProviders
+    ...usersProviders,
   ],
   exports: [
     ...usersProviders
