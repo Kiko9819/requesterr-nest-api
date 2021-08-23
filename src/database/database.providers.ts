666a5e70fb15b2c699a9d0331036c23c707f6019
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-24 00:57:20 +0300

    Refactoring database provider to use the ConfigService

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,21 +1,23 @@
+import { ConfigService } from "@nestjs/config";
 import { Sequelize } from "sequelize-typescript";
 import { models } from "./models";
 
 export const databaseProviders = [
     {
         provide: 'SEQUELIZE',
-        useFactory: async () => {
+        useFactory: async (config: ConfigService) => 
+          await (async () => {
             const sequelize = new Sequelize({
-              dialect: 'mysql',
-              host: 'localhost',
-              port: 3306,
-              username: 'root',
-              password: 'example',
-              database: 'example',
+              dialect: config.get('DB_DIALECT'),
+              host: config.get('DB_HOST'),
+              port: config.get('DB_PORT'),
+              username: config.get('DB_USERNAME'),
+              password: config.get('DB_PASSWORD'),
+              database: config.get('DB_NAME')
             });
             sequelize.addModels([...models]);
             await sequelize.sync();
             return sequelize;
-          },
+        })(), inject: [ConfigService]
     }
 ]
