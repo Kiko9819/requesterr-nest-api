20d7906c5d0bc0ca013be4d6ca915ac0b93af7b3
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-24 00:42:23 +0300

    Remove mail module, since we have it decorated with @Global

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MailModule } from 'src/mail/mail.module';
 import { UsersController } from './users.controller';
 import { usersProviders } from './users.providers';
 
 @Module({
-  imports: [
-    MailModule
-  ],
   controllers: [
     UsersController
   ],
