ac4315a03aa879251ccb937d92b7dc945c406cc6
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-25 00:01:42 +0300

    Add logic for user verificaiton, getting authenticated user and getting a cookie which will be used for storing Jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,24 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from './interfaces/token-payload.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         private jwtService: JwtService,
-        private usersService: UsersService
+        private usersService: UsersService,
+        private configService: ConfigService
     ) { }
 
-    async validateUser(username: string, pass: string): Promise<any> {
-        const user = await this.usersService.getUserByUsername(username);
-        const isPasswordMatching = await bcrypt.compare(pass, user.password);
+    getCookieWithJwtToken(userId: number) {
+        const payload: TokenPayload = {userId};
+        const token = this.jwtService.sign(payload);
+        return `Authentication=${token}; HttpOnly; Path=/;Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}`;
+    }
 
-        if (user && isPasswordMatching) {
-            const { password, ...result } = user;
-            return result;
+    async verifyPassword(plainTextPassword: string, hashedPassword: string) {
+        const isPasswordMatching = await bcrypt.compare(plainTextPassword, hashedPassword);
+
+        if(!isPasswordMatching) {
+            throw new UnauthorizedException();
         }
-        return null;
+    }
+
+    async getAuthenticatedUser(username: string, pass: string): Promise<any> {
+        try {
+            const user = await this.usersService.getUserByUsername(username);
+            const isPasswordMatching = this.verifyPassword(pass, user.password);
+    
+            if (user && isPasswordMatching) {
+                const { password, ...restOfUser } = user;
+
+                return restOfUser;
+            }
+        } catch(error) {
+            throw new UnauthorizedException();
+        }
     }
 
     async login(user: any) {
