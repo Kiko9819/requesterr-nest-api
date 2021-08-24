41df519d472fe80b2f2695cfcd97f5da79470021
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-25 00:38:17 +0300

    Send just the datavalues from the User that is attached to the request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, UseGuards, HttpCode, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import RequestWithUser from './interfaces/request-with-user.interface';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -11,15 +10,14 @@ export class AuthController {
     @HttpCode(200)
     @UseGuards(LocalAuthGuard)
     @Post('/login')
-    async login(@Req() request: RequestWithUser, @Res() response) {
-        const {user} = request;
-        
+    async login(@Req() request, @Res() response) {
+        const user = request.user.dataValues;
+
         const cookie = this.authService.getCookieWithJwtToken(user.id);
         response.setHeader('Set-Cookie', cookie);
 
         user.password = undefined;
         
-        // todo make this return only the needed data...
         return response.send(user);
     }
 }
