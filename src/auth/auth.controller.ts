3ef4708f71d3e47e398afb3499fdd4371864268a
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-25 00:03:00 +0300

    Anotate method with 200 status add response and setting header on the response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Post, UseGuards, HttpCode, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import RequestWithUser from './interfaces/request-with-user.interface';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -8,9 +8,18 @@ export class AuthController {
 
     constructor(private readonly authService: AuthService) {}
 
+    @HttpCode(200)
     @UseGuards(LocalAuthGuard)
     @Post('/login')
-    async login(@Request() req) {
-        return this.authService.login(req.user);
+    async login(@Req() request: RequestWithUser, @Res() response) {
+        const {user} = request;
+        
+        const cookie = this.authService.getCookieWithJwtToken(user.id);
+        response.setHeader('Set-Cookie', cookie);
+
+        user.password = undefined;
+        
+        // todo make this return only the needed data...
+        return response.send(user);
     }
 }
