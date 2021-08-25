1cd2725efbd262d22e20daf05d12df503652749a
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-25 22:46:16 +0300

    Group files into logical units

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,8 +1,9 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
+import { jwtConstants } from '../constants';
 import { Injectable } from '@nestjs/common';
-import { jwtConstants } from './constants';
 
+@Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
         super({
