bd9027c4881d47a633b795400578fcab56ac7b51
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-27 01:30:54 +0300

    Setup and test redis transport

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
 
-  constructor() {}
+  constructor(private appService: AppService) {}
+
+  @Get('say-hi')
+  async sayHid(){
+    return this.appService.getHello();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ClientOptions, ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private client: ClientProxy;
+  private logger = new Logger('Main');
+
+  constructor() {
+    const microservicesOptions: ClientOptions = {
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379'
+      }
+    };
+    this.client = ClientProxyFactory.create(microservicesOptions);
+  }
+
+  getHello() {
+    this.logger.log('Getting hello from microservice');
+    return this.client.send<string, any>('say-hi', {});
   }
 }
