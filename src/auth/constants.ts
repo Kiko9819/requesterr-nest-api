52f55950d9266fdaf104654aa05316ff3f4474ae
Kiko9819 <k.krastev.1998@gmail.com>
2021-08-28 17:54:38 +0300

    Remove constants

--- src/auth/constants.ts
+++
@@ -1,8 +0,0 @@
-/**
- * in a production system you must protect this key
- * using appropriate measures such as a secrets vault,
- * environment variable, or configuration service.
- */
-export const jwtConstants = {
-    secret: 'secretKey'
-};
