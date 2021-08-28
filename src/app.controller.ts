dc426fbf3d340376e1aef2579e389d037ff32c0f
Kiko9819 <k.krastev.1998@gmail.com>
2021-08-28 19:05:45 +0300

    Remove logic from app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
 
-  constructor(private appService: AppService) {}
-
-  // TODO: move these to a better place
-  // and make sure to have proper endpoint names
-
-  @Delete('/delete-token/:id')
-  deleteToken(@Param('id') id) {
-    return this.appService.deleteToken(id);
-  }
-
-  @Post('set-token')
-  setToken(@Body() body) {
-    return this.appService.setToken(body.key, body.value);
-  }
-
-  @Get('/get-token/:id')
-  getToken(@Param('id') id){
-    return this.appService.getToken(id);
-  }
+  constructor() {}
 }
