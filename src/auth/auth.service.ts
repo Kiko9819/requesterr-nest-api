6c2f6a266bd196bf293b02a1697675458f5627c6
Kiko9819 <k.krastev.1998@gmail.com>
2021-08-28 19:08:22 +0300

    Add methods for get, set and delete to the auth service for communication with the cache microservice

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,30 +4,44 @@ import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
 import { TokenPayload } from './interfaces/token-payload.interface';
+import { ClientOptions, ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { take } from 'rxjs';
 
 @Injectable()
 export class AuthService {
+
+    private client: ClientProxy;
+
     constructor(
         private jwtService: JwtService,
         private usersService: UsersService,
         private configService: ConfigService
-    ) { }
+    ) {
+        const microservicesOptions: ClientOptions = {
+            transport: Transport.REDIS,
+            options: {
+                url: 'redis://localhost:6379'// TODO make use of config service and get this from env
+            }
+        };
+        this.client = ClientProxyFactory.create(microservicesOptions);
+
+    }
 
     getCookieForLogOut() {
         return `Authentication=; HttpOnly; Path=/; Max-Age=0`;
     }
 
-    getCookieWithJwtToken(userId: number) {
-        const payload: TokenPayload = {userId};
+    getCookieWithJwtAccessToken(userId: number) {
+        const payload: TokenPayload = { userId };
         const token = this.jwtService.sign(payload, {
             secret: this.configService.get('JWT_ACCESS_TOKEN_SECRET'),
             expiresIn: `${this.configService.get('JWT_ACCESS_TOKEN_EXPIRATION_TIME')}s`
         });
-        return `Authentication=${token}; HttpOnly; Path=/;Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}`;
+        return `Authentication=${token}; HttpOnly; Path=/;Max-Age=${this.configService.get('JWT_ACCESS_TOKEN_EXPIRATION_TIME')}`;
     }
 
     getCookieWithJwtRefreshToken(userId: number) {
-        const payload: TokenPayload = {userId};
+        const payload: TokenPayload = { userId };
 
         const token = this.jwtService.sign(payload, {
             secret: this.configService.get('JWT_REFRESH_TOKEN_SECRET'),
@@ -42,10 +56,22 @@ export class AuthService {
         };
     }
 
+    getToken(id) {
+        return this.client.send('get-token', id);
+    }
+
+    setToken(key, value) {
+        return this.client.send('set-token', { key, value });
+    }
+
+    deleteToken(key) {
+        return this.client.send('delete-token', key);
+    }
+
     async verifyPassword(plainTextPassword: string, hashedPassword: string) {
         const isPasswordMatching = await bcrypt.compare(plainTextPassword, hashedPassword);
 
-        if(!isPasswordMatching) {
+        if (!isPasswordMatching) {
             throw new UnauthorizedException();
         }
     }
@@ -54,13 +80,13 @@ export class AuthService {
         try {
             const user = await this.usersService.getUserByUsername(username);
             const isPasswordMatching = this.verifyPassword(pass, user.password);
-    
+
             if (user && isPasswordMatching) {
                 const { password, ...restOfUser } = user;
 
                 return restOfUser;
             }
-        } catch(error) {
+        } catch (error) {
             throw new UnauthorizedException();
         }
     }
