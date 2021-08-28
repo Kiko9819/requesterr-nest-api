d3927d96d2e18162db4b724f6871cd6bff08974c
Kiko9819 <k.krastev.1998@gmail.com>
2021-08-28 16:01:24 +0300

    Add get,set, and delete in app service also configure the microservice create

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,36 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ClientOptions, ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
+import { take } from 'rxjs';
 
 @Injectable()
 export class AppService {
   private client: ClientProxy;
-  private logger = new Logger('Main');
 
   constructor() {
     const microservicesOptions: ClientOptions = {
       transport: Transport.REDIS,
       options: {
-        url: 'redis://localhost:6379'
+        url: 'redis://localhost:6379'// TODO make use of config service and get this from env
       }
     };
     this.client = ClientProxyFactory.create(microservicesOptions);
   }
 
-  getHello() {
-    this.logger.log('Getting hello from microservice');
-    return this.client.send<string, any>('say-hi', {});
+  getToken(id) {
+    return this.client.send('get-token', id).pipe(
+      take(1)
+    )
+  }
+
+  setToken(key, value) {
+    return this.client.send('set-token', {key, value}).pipe(
+      take(1)
+    )
+  }
+
+  deleteToken(key) {
+    return this.client.send('delete-token', key).pipe(
+      take(1)
+    );
   }
 }
